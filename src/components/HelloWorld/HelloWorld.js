b7ff2853a21c1da26065f592961e9add45a8ad78
cteixeiraVO <cteixeira@ventureoak.com>
2015-11-30 11:33:30 +0000

    started using stores + events + events test

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -2,13 +2,42 @@ import React from "react";
 import "./HelloWorld.scss";
 
 class HelloWorld extends React.Component {
-
     constructor(){
         super();
+        this.state = {
+            salutation : "Hello World",
+            punctuation: ""
+        }
+    }
+    handleChange(event) {
+        this.setState({salutation: event.target.value || 'Hello World'});
+    }
+    handleChangePunctuation(event){
+        this.setState({punctuation: event.target.value || ''});
     }
     render(){
-        return <div className="vo-reactEnv-helloWorld">
-            <h1>Hello World</h1>
+        return <div className="reactEnv-helloWorld">
+            <h1>{this.state.salutation} {this.state.punctuation}</h1>
+            <div>
+                <input
+                    val={this.state.salutation}
+                    placeholder="Saudation"
+                    name = "reactEnv-Salutation"
+                    className="reactEnv-Salutation"
+                    onChange={this.handleChange.bind(this)}
+                    />
+
+            </div>
+            <div>
+                <input
+                    val={this.state.punctuation}
+                    placeholder="Punctuation"
+                    name = "reactEnv-Punctuation"
+                    className="reactEnv-Punctuation"
+                    onChange={this.handleChangePunctuation.bind(this)}
+                    />
+            </div>
+
 
         </div>
     }
--- tests/unit/components/HelloWorld-test.js
+++ tests/unit/components/HelloWorld-test.js
@@ -6,10 +6,38 @@ import TestUtils from 'react-addons-test-utils';
 const HelloWorld = require('../../../src/components/HelloWorld/HelloWorld.js');
 
 describe('HelloWorld', () => {
-    it('must say HelloWorld', function() {
+    it('must initially say HelloWorld', () => {
         var Component = TestUtils.renderIntoDocument(<HelloWorld></HelloWorld>);
-        var element = TestUtils.findRenderedDOMComponentWithClass(Component, 'vo-reactEnv-helloWorld');
+        var element = TestUtils.findRenderedDOMComponentWithClass(Component, 'reactEnv-helloWorld');
         expect(element).toBeDefined();
-        expect(element.getDOMNode().innerHTML).toMatch(/Hello World/g);
+        expect(element.innerHTML).toMatch(/Hello World/g);
     });
+    it('must change saudation',() => {
+        var Component = TestUtils.renderIntoDocument(<HelloWorld></HelloWorld>);
+        var input = TestUtils.findRenderedDOMComponentWithClass(Component,'reactEnv-Salutation');
+        expect(input).toBeDefined();
+        TestUtils.Simulate.change(input,{
+            target :{
+                value: "Something"
+            }
+        },input);
+        var element = TestUtils.findRenderedDOMComponentWithClass(Component, 'reactEnv-helloWorld');
+        expect(element).toBeDefined();
+        expect(element.innerHTML).toMatch(/Something/g);
+    });
+    it('must change punctuation',() => {
+        var Component = TestUtils.renderIntoDocument(<HelloWorld></HelloWorld>);
+        var input = TestUtils.findRenderedDOMComponentWithClass(Component,'reactEnv-Punctuation');
+        expect(input).toBeDefined();
+        TestUtils.Simulate.change(input,{
+            target :{
+                value: "!!!!"
+
+            }
+        },input);
+        var element = TestUtils.findRenderedDOMComponentWithClass(Component, 'reactEnv-helloWorld');
+        expect(element).toBeDefined();
+        expect(element.innerHTML).toMatch(/\!\!\!\!/g);
+
+    })
 });
